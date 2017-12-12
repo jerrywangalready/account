c83f59c74099b37d522985ae191ddc7db452ece7
jerrywangalready <sunnyamos@163.com>
2017-12-12 23:13:39 +0800

    权限功能

--- src/main/webapp/account/settings/js/settings.js
+++ src/main/webapp/account/settings/js/settings.js
@@ -12,6 +12,18 @@ settings.js.init = function () {
         var labelName = $(this).attr("labelName");
         comm.js.hashAppand("ln",labelName);
     });
+    // 获取登录用户权限
+    $.post(path+"/settings/getRole.do",{},function (data) {
+        for(var i=0;i<data.length;i++){
+            if(data[i] == "superadmin"){
+                $("li[labelName=maintainCard],li[labelName=manageAccount],li[labelName=memberMaintain]").removeClass("hidden");
+                return false;
+            }
+            if(data[i] == "card_owner"){
+                $("li[labelName=memberMaintain]").removeClass("hidden");
+            }
+        }
+    });
 
     var label = getParameter(location.hash,"ln","changePassword");
     $(".right-box").load(path+"/settings/"+label+".do");
