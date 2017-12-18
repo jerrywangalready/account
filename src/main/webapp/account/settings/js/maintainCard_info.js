9abf3c72e00c33758636facd10d8d4629f9a7b69
xiaoxingxingxing <1065639166@qq.com>
2017-12-18 21:17:11 +0800

    bug版

---
+++ src/main/webapp/account/settings/js/maintainCard_info.js
@@ -0,0 +1,83 @@
+jQuery.namespace("maintainCard_info");
+$(function () {
+
+    maintainCard_info.js.init();
+});
+
+maintainCard_info.js = {};
+
+maintainCard_info.js.init = function () {
+
+    //初始化列表数据
+    maintainCard_info.js.query();
+
+};
+
+maintainCard_info.js.query = function(){
+
+    var collector = $("#query_box").collector();
+    $.ajax({
+        type:'POST',
+        url:path+"/maintainCard/queryList.do",
+        contentType:'application/json',
+        data:JSON.stringify(collector),
+        success:function (data) {
+            console.info(data);
+            var html = template('demo_cards',{'list':data});
+            console.info(html);
+            $("#table_div").html(html);
+            //初始化页码按钮
+            //$("#page-bar").page(data);
+        }
+    });
+
+};
+
+//增加
+maintainCard_info.js.addCard = function () {
+    layer.open({
+        type:2,
+        title:" 新增饭卡",
+        area:['375px','280px'],
+        scrollbar:false,
+        content:[path + '/maintainCard_info/addCardInfo.do', 'no']
+    });
+};
+
+//修改
+maintainCard_info.js.changeCard = function(uuid){
+/*    console.info("xiugai yemian ");
+    layer.open({
+        type:2,
+        title:" 新增饭卡",
+        area:['500px','450px'],
+        scrollbar:false,
+        content:[path + '/maintainCard_info/changeCardInfo.do?uuid='+uuid, 'no']
+    });*/
+    //comm.js.hashAppand("mc","cardDetail");
+    comm.js.hashAppand("mc",uuid);
+};
+
+//删除
+maintainCard_info.js.deleteCard = function (uuid) {
+    var param = {};
+    param.uuid = uuid;
+    console.info(param);
+
+    $.ajax({
+        type:'POST',
+        url:path+"/maintainCard_info/deleteCardInfo.do",
+        //contentType:'application/json',
+       // data:JSON.stringify(param),
+        data:{uuid:uuid},
+        success:function (data) {
+
+            if (data == "success"){
+                layer.msg("删除成功！");
+                maintainCard_info.js.init();
+            }else{
+                layer.msg("删除失败！");
+            }
+        }
+    });
+};
